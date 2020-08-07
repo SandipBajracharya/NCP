e8c7bef9390f2d649a033b8781e3d26ad724165e
Sandeep Bajracharya <sndp106.sb@gmail.com>
2020-08-07 10:33:23 +0545

    gitignore update

--- resources/views/inc/topDesign.blade.php
+++ resources/views/inc/topDesign.blade.php
@@ -1,12 +1,12 @@
 <div class="d-flex py-2" style="background-color:#48c7eb;">
     <div class="col-3 " style="padding-left: 120px;">
-        <img src="public/storage/top_image/congress_gif.gif" class="img-fluid" alt="party_flag">
+        <img src="/storage/top_image/congress_gif.gif" class="img-fluid" alt="party_flag">
     </div>
     <div class="col-6 ">
-        <img src="public/storage/top_image/Nepali_congress.jpg" class="img-fluid" alt="header_image">
+        <img src="/storage/top_image/Nepali_congress.jpg" class="img-fluid" alt="header_image">
     </div>
     <div class="col-3 "style="padding-left: 67px;">
-        <img src="public/storage/top_image/flag.gif" class="img-fluid" alt="national_image">
+        <img src="/storage/top_image/flag.gif" class="img-fluid" alt="national_image">
     </div>
     {{-- <h1 class="text-center"> Here is image design for the site</h1> --}}
 </div>
