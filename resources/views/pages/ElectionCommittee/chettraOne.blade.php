18ba214bf8f81d5a6a4e4e5cf33fb0c3be0effcb
Sandeep Bajracharya <sndp106.sb@gmail.com>
2020-08-07 10:42:32 +0545

    view  update

--- resources/views/pages/ElectionCommittee/chettraOne.blade.php
+++ resources/views/pages/ElectionCommittee/chettraOne.blade.php
@@ -6,7 +6,7 @@
         {{-- @foreach($users as $user) --}}
             <div class="col-md-3 col-lg-3 mb-3">
                 <div class="card">
-                    <img class="card-img-top p-1" src="public/storage/Leaders/Bp_koirala.jpg" alt="person1" style="height: 15em;">
+                    <img class="card-img-top p-1" src="/storage/Leaders/Bp_koirala.jpg" alt="person1" style="height: 15em;">
                     <div class="card-body p-2">
                         <p class="card-text text-secondary"> Name: Person 1</p>
                     </div>
